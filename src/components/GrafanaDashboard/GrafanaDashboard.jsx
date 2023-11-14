c2a59a39e48ad095b2ca35d2571e7d7a54db5434
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-11-14 18:45:56 +0500

    data dsiplay on card charts

--- src/components/GrafanaDashboard/GrafanaDashboard.jsx
+++ src/components/GrafanaDashboard/GrafanaDashboard.jsx
@@ -1,11 +1,62 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 import LineChart from "./LineChart";
 import Search from "../../assets/Search.svg";
 import Sidebar from "../sidebar/Sidebar";
 import HeatmapChart from "./HeatMap";
 import CardChart from "./CardChart";
 import "./styles.css";
+import mqtt from "mqtt";
+import SingleLineChart from "./SingleLineChart"
 const GrafanaDashbaord = () => {
+  const username = "admin";
+  const password = "o99Y7YndJy0YwNlW3DkC9DZk0";
+  const brokerUrl = "ws://tag4track.com:8083/mqtt";
+
+  const [topic, setTopic] = useState([]);
+  useEffect(() => {
+    const client = mqtt.connect(brokerUrl, {
+      clientId: "emqx_ODY3MD",
+      username: username,
+      password: password,
+    });
+
+    client.on("connect", () => {
+      console.log("Connected to MQTT broker");
+      const topic2 = "kbeacon/publish/94A408B91ABC";
+
+      client.subscribe(topic2, (err) => {
+        if (!err) {
+          console.log(`Subscribed to topic: ${topic2}`);
+        }
+      });
+    });
+
+    client.on("message", (topic2, message) => {
+      try {
+        const parsedMessage = JSON.parse(message.toString());
+
+        if (parsedMessage.obj) {
+          // console.log(`Received message on topic ${topic2}:`, parsedMessage.obj);
+          setTopic(parsedMessage.obj);
+        } else {
+          console.log(
+            `Received message on topic ${topic2} does not contain an 'obj' property.`
+          );
+        }
+      } catch (error) {
+        console.error(`Error parsing message on topic ${topic2}:`, error);
+      }
+    });
+
+    client.on("error", (err) => {
+      console.error("MQTT error:", err);
+    });
+
+    return () => {
+      client.end();
+    };
+  }, []);
   return (
     <div className="grafanaMain">
       <div
@@ -42,44 +93,26 @@ const GrafanaDashbaord = () => {
             gap: "10px",
             marginBottom: "30px",
             alignItems: "center",
+            width:"100%",
+            flexWrap:"wrap"
           }}
         >
-          <CardChart
-            mac="3R804-FCD6"
-            temp="33 C"
-            bgColor="#c26786"
-            lineColor="#e8d3da"
-            areaColor=" #e3b6c5"
-            sampleData={[
-              [13279600000, 30.95],
-              [-132746000000, 22.34],
-              [1327400000, 12.18],
-            ]}
-          />
-          <CardChart
-            mac="3R807-FC7F"
-            temp="25 C"
-            bgColor="#78BF9F"
-            lineColor="#D8F0E5"
-            areaColor=" #B6E3CF"
-            sampleData={[
-              [23456, 30.95],
-              [-786, -22.34],
-              [2378, 12.18],
-            ]}
-          />
-          <CardChart
-            mac="3R907-FC8F"
-            temp="35 C"
-            bgColor="#bd5e5e"
-            lineColor="#e6c3c3"
-            areaColor="#bf7878"
-            sampleData={[
-              [22, 30.95],
-              [12, 7.34],
-              [28, -21.18],
-            ]}
-          />
+          {topic?.slice(0, 6).map((i, index) => (
+            <div key={index}>
+              <CardChart
+                mac={i.dmac}
+                temp={`${i.temp}°C`}
+                bgColor="#c26786"
+                lineColor="#e8d3da"
+                areaColor=" #e3b6c5"
+                sampleData={[
+                  [13279600000, 30.95],
+                  [-132746000000, 22.34],
+                  [1327400000, 12.18],
+                ]}
+              />
+            </div>
+          ))}
         </div>
         <div>
           <LineChart />
@@ -90,6 +123,12 @@ const GrafanaDashbaord = () => {
           }}
         >
           <HeatmapChart />
+        </div>
+        <div          style={{
+            marginTop: "30px",
+          }}>
+        <SingleLineChart data={topic} />
+
         </div>
       </div>
     </div>
