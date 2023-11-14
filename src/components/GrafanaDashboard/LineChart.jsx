73b8798130e21b7960d7ef719a210b310a76d397
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-11-14 18:48:47 +0500

    unneccessary usestate removal in line chart

--- src/components/GrafanaDashboard/LineChart.jsx
+++ src/components/GrafanaDashboard/LineChart.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactApexChart from "react-apexcharts";
 import "./styles.css"
 const LineChart = () => {
-  const [chartData, setChartData] = useState({
+
+
+  const options = {
     series: [
       {
         name: "Line 1",
@@ -25,78 +27,76 @@ const LineChart = () => {
         color: "green",
       },
     ],
-    options: {
-      chart: {
-        height: 350,
-        type: "line",
-        dropShadow: {
-          enabled: true,
-          color: "#000",
-          top: 18,
-          left: 7,
-          blur: 10,
-          opacity: 0.2,
+    chart: {
+      height: 350,
+      type: "line",
+      dropShadow: {
+        enabled: true,
+        color: "#000",
+        top: 18,
+        left: 7,
+        blur: 10,
+        opacity: 0.2,
+      },
+      toolbar: {
+        show: true,
+        tools: {
+          zoom: false,
+          zoomin: false,
+          zoomout: false,
+          selection: false,
+          pan: false,
+          reset:false
         },
-        toolbar: {
-          show: true,
-          tools: {
-            zoom: false,
-            zoomin: false,
-            zoomout: false,
-            selection: false,
-            pan: false,
-            reset:false
-          },
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      title: {
-        text: "Average High & Low Temperature",
-        align: "left",
-      },
-      grid: {
-        borderColor: "#e7e7e7",
-        row: {
-          colors: ["#f3f3f3", "transparent"],
-          opacity: 0.5,
-        },
-      },
-      markers: {
-        size: 1,
-      },
-      xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
-        title: {
-          text: "Month",
-        },
-      },
-      yaxis: {
-        title: {
-          text: "Temperature",
-        },
-        min: 5,
-        max: 40,
-      },
-      legend: {
-        position: "top",
-        horizontalAlign: "right",
-        floating: true,
-        offsetY: -25,
-        offsetX: -5,
       },
     },
-  });
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    title: {
+      text: "Average High & Low Temperature",
+      align: "left",
+    },
+    grid: {
+      borderColor: "#e7e7e7",
+      row: {
+        colors: ["#f3f3f3", "transparent"],
+        opacity: 0.5,
+      },
+    },
+    markers: {
+      size: 1,
+    },
+    xaxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+      title: {
+        text: "Month",
+      },
+    },
+    yaxis: {
+      title: {
+        text: "Temperature",
+      },
+      min: 5,
+      max: 40,
+    },
+    legend: {
+      position: "top",
+      horizontalAlign: "right",
+      floating: true,
+      offsetY: -25,
+      offsetX: -5,
+    },
+  }
 
   return (
     <div id="chart" className="chartWrap">
       <ReactApexChart
-        options={chartData.options}
-        series={chartData.series.map((data) => ({
+        options={options}
+        series={options.series.map((data) => ({
           name: data.name,
           data: data.data,
         }))}
